8a35dc06c212a2040a09d46ce028a4f0713ce569
akima <akm2000@gmail.com>
2013-07-19 23:46:48 +0900

    テストがパスしなくなっていたので修正しました

--- lib/moped_mapping/session_context_ext.rb
+++ lib/moped_mapping/session_context_ext.rb
@@ -32,7 +32,7 @@ module MopedMapping
       # #sharding-commands
       :shardCollection,
       # #instance-administration-commands
-      :drop, :create, :convertToCapped,
+      :drop, :create, # :convertToCapped,
       :dropIndexes, :compact, :collMod, :reIndex,
 
       # #diagnostic-commands
@@ -40,6 +40,16 @@ module MopedMapping
     ].freeze
 
     COLLECTION_COMMAND_NAMES = (collection_command_names + collection_command_names.map(&:to_s).map(&:freeze)).freeze
+
+    command_args_array = [
+      # [:cloneCollection, :from], # このfromはホスト名を指定するので、対象外
+      [:cloneCollectionAsCapped, :toCollection],
+    ].map(&:freeze).freeze
+
+    COMMAND_ARGS_ARRAY = (
+      command_args_array +
+      command_args_array.map{|args| args.map(&:to_s).map(&:freeze).freeze }
+    ).freeze
 
     full_name_command_names = [
       :renameCollection, :to
@@ -54,6 +64,16 @@ module MopedMapping
         command[name] = MopedMapping.mapped_name(database, command[name])
         done = true
         break
+      end
+      unless done
+        COMMAND_ARGS_ARRAY.each do |args|
+          next unless command.key?(args.first)
+          args.each do |arg|
+            command[arg] = MopedMapping.mapped_name(database, command[arg])
+          end
+          done = true
+          break
+        end
       end
       unless done
         FULL_NAME_COMMAND_NAMES.each do |name|
