b89a895798a34fd663c8d49d00686fd6edfa7559
Shinichirow KAMITO <s-kamito@groovenauts.jp>
2013-11-05 14:02:57 +0900

    クエリ実行時にページング(GET_MORE)されているところでバージョンが付与されていなかったので修正しました。

--- lib/moped_mapping/node_ext.rb
+++ lib/moped_mapping/node_ext.rb
@@ -7,12 +7,19 @@ module MopedMapping
       klass.module_eval do
         alias_method :query_without_mapping, :query
         alias_method :query, :query_with_mapping
+        alias_method :get_more_without_mapping, :get_more
+        alias_method :get_more, :get_more_with_mapping
       end
     end
 
     def query_with_mapping(database, collection, selector, options = {}, &block)
       collection = MopedMapping.mapped_name(database, collection)
       return query_without_mapping(database, collection, selector, options, &block)
+    end
+
+    def get_more_with_mapping(database, collection, selector, options = {}, &block)
+      collection = MopedMapping.mapped_name(database, collection)
+      return get_more_without_mapping(database, collection, selector, options, &block)
     end
   end
 end
